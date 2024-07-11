aeaa56c37ba10a10c40552b391794546cd510dd1
Hearing0 <gameblamer0@gmail.com>
2024-07-11 09:11:55 -0400

    Merge & Update: Load Config param & Beamforming
    Merged Load Configuration branch and added Beamforming (applying phasing vector)

--- src/clear_freq_search.c
+++ src/clear_freq_search.c
@@ -9,13 +9,13 @@
 #include <string.h>
 #include <math.h>
 #include "../utils/ini_parser/ini.c"
+#include "../utils/read_config.c"
 
+
+// Define Constants
 #define PI 3.14159265358979323846
-
-//
-int C = 3e8;
-bool verbose = false;
-
+double C = 3e8;//* pow(10, 8.0);
+bool verbose = true;
 // #define RESTRICT_FILE = '/home/radar/repos/SuperDARN_MSI_ROS/linux/home/radar/ros.3.6/tables/superdarn/site/site.sps/restrict.dat.inst'
 
 
@@ -26,6 +26,8 @@ bool verbose = false;
 */
 
 
+
+
 typedef struct {
     int *antenna_list;
     int num_antennas;
@@ -33,9 +35,15 @@ typedef struct {
     double x_spacing;
     int usrp_rf_rate;
     int usrp_fcenter;
-} SampleMetaData;
+} sample_meta_data;
 
-void read_input_data(const char *filename, SampleMetaData *meta_data, fftw_complex ***raw_samples) {
+typedef struct {
+    double *restricted_freq;
+    double *clear_freq_range;
+} freq_data;
+
+
+void read_input_data(const char *filename, sample_meta_data *meta_data, double **clear_freq_range, fftw_complex ***raw_samples) {
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
         perror("Error opening file");
@@ -49,8 +57,19 @@ void read_input_data(const char *filename, SampleMetaData *meta_data, fftw_compl
         if (sscanf(line, "number_of_samples: %d", &meta_data->number_of_samples) == 1) continue;
         if (sscanf(line, "usrp_rf_rate: %d", &meta_data->usrp_rf_rate) == 1) continue;
         if (sscanf(line, "usrp_fcenter: %d", &meta_data->usrp_fcenter) == 1) continue;
-        if (sscanf(line, "x_spacing: %lf", &meta_data->x_spacing) == 1) continue;
-
+        // if (sscanf(line, "x_spacing: %lf", &meta_data->x_spacing) == 1) continue;
+        if (strncmp(line, "clear_freq_range:", 15) == 0) {
+            clear_freq_range = realloc(clear_freq_range, 2 * sizeof(double));
+            int i = 0;
+            char *token = strtok(line + 16, ",");
+            while (token != NULL) {
+                (*clear_freq_range)[i] = atof(token);
+                i++;
+            }
+            continue;
+        }
+        
+        // Antenna List
         if (strncmp(line, "antenna_list:", 13) == 0) {
             char *token = strtok(line + 14, ",");
             while (token != NULL) {
@@ -69,6 +88,7 @@ void read_input_data(const char *filename, SampleMetaData *meta_data, fftw_compl
             continue;
         }
 
+        // Raw Samples
         if (strncmp(line, "raw_samples:", 12) == 0) {
             // Allocate mem
             *raw_samples = (fftw_complex **)fftw_malloc(meta_data->num_antennas * sizeof(fftw_complex *));
@@ -100,21 +120,63 @@ void read_input_data(const char *filename, SampleMetaData *meta_data, fftw_compl
 }
 
 /**
+ * @brief  Loads in the beam configuration from array_config.ini. 
+ * @note   By DF
+ * @param  *n_beams:    Number of beams
+ * @param  *beam_sep:   Angle Offset between beams (in degrees)
+ * @retval None
+ */
+void load_beam_config(double *x_spacing, int *n_beams, double *beam_sep){
+    const char *config_path = "../Freq_Server/utils/clear_freq_input/array_config.ini";
+    Config config;
+
+    if (ini_parse(config_path, config_ini_handler, &config) < 0) {
+        printf("Can't load 'config.ini'\n");
+        return;
+    }
+
+    *x_spacing = config.array_info.x_spacing;
+    *n_beams = config.array_info.nbeams;
+    *beam_sep = config.array_info.beam_sep;
+}
+
+/**
+ * @brief  Calculates Beam Azimuth Angle.
+ * @note   By DF
+ * @param  n_beams:     Total number of beams
+ * @param  beam_num:    Beam number for calculating Beam Azimuth
+ * @param  beam_sep:    Beam Seperation (in degrees)
+ * @retval Returns the Beam Azumuth Angle (in radians)
+ */
+double calc_beam_angle(int n_beams, int beam_num, double beam_sep) {
+    // Calculate Beamforming shift
+    double center_beam = (n_beams - 1) / 2;
+
+    // Calculate Beam Azimuth
+    double b_azi = ((beam_num - center_beam) * beam_sep) * (PI / 180);
+    if (verbose){
+        printf("n_beams: %d, beam_num: %d, beam_sep: %lf\n", n_beams, beam_num, beam_sep);
+        printf("    beam = %lf degree", (b_azi * 180 / PI));
+    }
+    return b_azi;
+}
+
+/**
  * @brief  Calculates phase increment between antennas to produce a mainlobe sterring 
  *         of beam_angle at center_frequency.
- * 
- * @param  beam_angle: Distrubution of the beam (in radians)
- * @param  center_frequency: Frequency at the center of the beam; 
+ * @note   By DF
+ * @param  beam_angle:          Distrubution of the beam (in radians)
+ * @param  center_frequency:    Frequency at the center of the beam; 
  * * used to phase-shift allign the other frequencys (in Hz)
- * @param  x_spacing: Spacing in-between antennas
+ * @param  x_spacing:           Spacing in-between antennas
  *         
- * @retval phase_shift: Phase shift (in degrees)
+ * @retval Returns the Phase Shift (in degrees)
  */
-double calc_phase_increment(double beam_angle, double center_frequency, double x_spacing) {
+float calc_phase_increment(float beam_angle, double center_frequency, double x_spacing) {
     double wavelength = C / center_frequency;
     double phase_shift = (2 * PI * x_spacing * sin(beam_angle)) / wavelength;
     if (verbose) {
-        printf("center_freq: %lf x_spacing: %lf \nphase_shift: %lf degree", center_frequency, x_spacing, phase_shift * 180 / PI);
+        printf("center_freq: %lf\nx_spacing: %lf\nphase_shift: %lf degree\n", center_frequency, x_spacing, phase_shift * 180 / PI);
     }
     return phase_shift; 
 }
@@ -142,12 +204,13 @@ void fft_clrfreq_samples(fftw_complex *beamformed_samples, int num_samples, fftw
     fftw_destroy_plan(plan);
 }
 
+
 void find_clear_freq(fftw_complex *spectrum_power, double *freq_vector, double start_freq, double end_freq, double clear_bw, double *tfreq, double *noise) {
 
 }
 
 // HACK apply efficient matrix multi via cblas_dgemm
-void calc_clear_freq_on_raw_samples(fftw_complex **raw_samples, SampleMetaData *meta_data, double *restricted_frequencies, double *clear_freq_range, double beam_angle, double smsep) {
+void calc_clear_freq_on_raw_samples(fftw_complex **raw_samples, sample_meta_data *meta_data, double *restricted_frequencies, double *clear_freq_range, double beam_angle, double smsep) {
     // Extract meta data
     int num_samples = meta_data->number_of_samples;
     int *antennas = meta_data->antenna_list;
@@ -165,12 +228,12 @@ void calc_clear_freq_on_raw_samples(fftw_complex **raw_samples, SampleMetaData *
     double *freq_vector = (double*) malloc(sizeof(double) * num_samples);
     
     if (!phasing_vector || !beamformed_samples || !spectrum_power || !freq_vector) {
-        perror("Error allocating memory");
+        perror("Error allocating memory.\n");
         exit(EXIT_FAILURE);
     }
 
     // Calculate and Apply phasing vector
-    double phase_increment = calc_phase_increment(beam_angle, (clear_freq_range[0] + clear_freq_range[1]) / 2 * 1000, meta_data->x_spacing);
+    float phase_increment = calc_phase_increment(beam_angle, (clear_freq_range[0] + clear_freq_range[1]) / 2, meta_data->x_spacing);
     printf("phase_increment: %lf\n", phase_increment);
     // phase_increment = .000738
     
@@ -186,7 +249,10 @@ void calc_clear_freq_on_raw_samples(fftw_complex **raw_samples, SampleMetaData *
             }
         }
     }
+    printf("antenna[13]: %d\n", antennas[13]);
+    // Antennas[13] = 18
     printf("phasing_vector[13]: %f + %fi\n", creal(phasing_vector[13]), cimag(phasing_vector[13]));
+    // phasing_vector[13] = 0.999912 + 0.013288i
 
     // Apply beamforming
     for (int i = 0; i < num_samples; i++) {
@@ -194,10 +260,14 @@ void calc_clear_freq_on_raw_samples(fftw_complex **raw_samples, SampleMetaData *
         double imag_sum = 0.0;
         
         for (int aidx = 0; aidx < meta_data->num_antennas; aidx++) {
-            double real_sample = creal(raw_samples[aidx][i]);       // TODO: convert from fftw to __complex___ 
+            double real_sample = creal(raw_samples[aidx][i]);       
             double imag_sample = cimag(raw_samples[aidx][i]);
             double real_phase = creal(phasing_vector[aidx]);
             double imag_phase = cimag(phasing_vector[aidx]);
+            if (i == 2499) {
+                printf("sample[%d][2499]    = %f + %fi\n", aidx, real_sample, imag_sample);
+                printf("phase[%d]           = %f + %fi\n", aidx, real_phase, imag_phase);
+            }
 
             real_sum += real_sample * real_phase - imag_sample * imag_phase;
             imag_sum += real_sample * imag_phase + imag_sample * real_phase;
@@ -206,7 +276,11 @@ void calc_clear_freq_on_raw_samples(fftw_complex **raw_samples, SampleMetaData *
 
         beamformed_samples[i] = real_sum + I * imag_sum;
     }
-    printf("beamformed[2499]:  %f + %fi\n", creal(beamformed_samples[2499]), cimag(beamformed_samples[2499]));
+    printf("beamformed[2499]    = %f + %fi\n", creal(beamformed_samples[2499]), cimag(beamformed_samples[2499]));
+    // beamformed[2499] =  70.466100 + -169.215264i
+    // TODO: Verify answer
+
+
 
     // // Spectral Estimation
     // fft_clrfreq_samples(beamformed_samples, num_samples, spectrum_power);
@@ -237,7 +311,7 @@ void calc_clear_freq_on_raw_samples(fftw_complex **raw_samples, SampleMetaData *
     // // Output results
     // printf("Clear Frequency: %f, Noise: %f\n", tfreq, noise);
 
-    printf("Finished Clear Freq!");
+    printf("Finished Clear Freq!\n");
     
     fftw_free(phasing_vector);
     fftw_free(beamformed_samples);
@@ -252,22 +326,29 @@ int main() {
 
     // HACK: Setup file_path environment variable
     const char *input_file_path = "../Freq_Server/utils/clear_freq_input/clrfreq_dump.1.txt";
-    const char *config_file_path = "../Freq_Server/utils/clear_freq_input/array_config.ini";
     // const char *output_file_path = "../utils/txt_output/result.txt";
 
     // Initial Data Variables
-    SampleMetaData meta_data = {0};
+    sample_meta_data meta_data = {0};
     fftw_complex **raw_samples = NULL;
+    int n_beams, beam_num;
+    double beam_sep;
+    freq_data freq_data;
 
-    // Load raw_samples and meta_data
-    read_input_data(input_file_path, &meta_data, &raw_samples);
-       
+    // Load in data for Clear Freq Calculation
+    read_input_data(input_file_path, &meta_data, &freq_data.clear_freq_range, &raw_samples);
+
+    // Load in data for Beam Angle Calculation
+    load_beam_config(&meta_data.x_spacing, &n_beams, &beam_sep);
+    beam_num = 1;
+
     printf("num_samples: %d\nx_spacing: %lf\nusrp_rf_rate: %d\nusrp_fcenter: %d\n",
         meta_data.number_of_samples,
         meta_data.x_spacing,
         meta_data.usrp_rf_rate,
         meta_data.usrp_fcenter
-    );
+    );       
+    printf("n_beams: %d\nbeam_sep: %f\nbeam_num: %d\n", n_beams, beam_sep, beam_num);
 
     // Check last sample
     // fftw_complex sample = raw_samples[meta_data.num_antennas - 1][meta_data.number_of_samples - 1];
@@ -278,11 +359,11 @@ int main() {
     // XXX: Define other parameters
     double restricted_frequencies[] = { 0,0 };
     double clear_freq_range[] = { 25 * pow(10,3), 40 * pow(10,3) };
-    double beam_angle = 0.08482300164692443; //0.08482300164692443
-    double smsep = 1 / (2 * 250 * pow(10, 3)); // ~4 ms
+    // double beam_angle = calc_beam_angle(n_beams, beam_num, beam_sep);    // 
+    double beam_angle = 0.08482300164692443;        // in radians
+    double smsep = 1 / (2 * 250 * pow(10, 3));      // ~4 ms
 
-
-    // Call calc_clear_freq_on_raw_samples
+    // Find Clear Frequency
     calc_clear_freq_on_raw_samples(
         raw_samples, &meta_data, restricted_frequencies, 
         clear_freq_range, beam_angle, smsep);
@@ -294,6 +375,7 @@ int main() {
     fftw_free(raw_samples);
     free(meta_data.antenna_list);
     
+
     // Print processing time; Stopwatch End
     // double t2 = dsecnd();
     // printf("Time for Clear Freq Search: %lf", (t2-t1));
---
+++ src/tests/beam_angle_test.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+// Define PI if not already defined
+#ifndef PI
+#define PI 3.14159265358979323846
+#endif
+
+int verbose = 1; // Set to 1 for verbose output
+
+double calc_beam_angle(int n_beams, int beam_num, double beam_sep) {
+    // Calculate Beamforming shift
+    double center_beam = (n_beams - 1) / 2;
+
+    // Calculate Beam Azimuth
+    double b_azi = ((beam_num - center_beam) * beam_sep) * (PI / 180);
+    if (verbose) {
+        printf("n_beams: %d, beam_num: %d, beam_sep: %lf\n", n_beams, beam_num, beam_sep);
+        printf("    beam = %lf degree\n", (b_azi * 180 / PI));
+    }
+    return b_azi;
+}
+
+int main() {
+    // Example usage of calc_beam_angle
+    double angle = calc_beam_angle(16, 3, 3.24);
+    printf("Calculated beam angle: %lf radians\n", angle);
+    return 0;
+}
