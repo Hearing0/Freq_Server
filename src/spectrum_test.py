Fix: Spectrum Plot Frequency Scaling
Fixed Spectrum Plot freq scaling to MHz instead of Hz

src/spectrum_test.py
@@ -16,16 +16,20 @@ if 'Frequency' not in data.columns or 'Power' not in data.columns:
     print(f"Available columns: {data.columns}")
     raise ValueError("The required columns 'Frequency' and 'Power' are not present in the CSV file.")
 
-# Plot the spectrum data
-Power = data['Power'].to_numpy()
-Freq = data['Frequency'].to_numpy()
+# Plot the spectrum data (Freq in MHz)
+# data['Power'] = data['Power'] 
+Power = data['Power'].to_numpy() 
+Freq = data['Frequency'].to_numpy() / 1e6
+
 
 plt.plot(Freq, Power)
-plt.xlabel('Frequency (Hz)')
+plt.xlabel('Frequency (MHz)')
 plt.ylabel('Power')
 plt.title('Spectrum Analysis')
 plt.grid(True)
 plt.ylim(0,10000)
+# plt.xticks(np.arange(min(Freq), max(Freq) + 1e3, 1e6))
+plt.yticks(np.arange(0, 11e3, 2000))
 # TODO: scale freq to 1e6 instead
 
 